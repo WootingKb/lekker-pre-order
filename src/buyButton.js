edc71b46e352b09594bac65cb7d9c37b7e69183c
Jeroen Langelaan <jeroen@wooting.nl>
2019-09-26 18:17:52 +0800

    try adding select

--- src/buyButton.js
+++ src/buyButton.js
@@ -18,7 +18,23 @@ const iframeId = 'd053a7e3-e770-4626-9048-b347b8c134e1'
 const usId = 'product-component-a9f54d95b46'
 const euId = 'product-component-527947ee585'
 document.getElementById(iframeId).innerHTML += `
-<div id="${usId}"></div>
+<div id="${usId}"><div class="shopify-buy__product__variant-selectors" data-element="product.options"><div class="shopify-buy__option-select" data-element="option.option">
+<label class="shopify-buy__option-select__label " data-element="option.label">Language layout</label>
+  <div class="shopify-buy__option-select-wrapper" data-element="option.wrapper">
+  <select class="shopify-buy__option-select__select" name="Language layout" data-element="option.select">
+      <option selected="" value="United States - US ANSI">United States - US ANSI</option>
+  </select>
+  <svg class="shopify-buy__select-icon" data-element="option.selectIcon" viewBox="0 0 24 24"><path d="M21 5.176l-9.086 9.353L3 5.176.686 7.647 12 19.382 23.314 7.647 21 5.176z"></path></svg>
+</div>
+</div><div class="shopify-buy__option-select" data-element="option.option">
+<label class="shopify-buy__option-select__label " data-element="option.label">Switch type</label>
+  <div class="shopify-buy__option-select-wrapper" data-element="option.wrapper">
+  <select class="shopify-buy__option-select__select" name="Switch type" data-element="option.select">
+      <option selected="" value="Lekker - Linear65">Lekker - Linear65</option>
+  </select>
+  <svg class="shopify-buy__select-icon" data-element="option.selectIcon" viewBox="0 0 24 24"><path d="M21 5.176l-9.086 9.353L3 5.176.686 7.647 12 19.382 23.314 7.647 21 5.176z"></path></svg>
+</div>
+</div></div></div>
 <div id="${euId}"></div>
 <br>
 <a id="buyButtonLink"></a>`
